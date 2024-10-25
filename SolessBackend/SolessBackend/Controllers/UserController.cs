b233f7f00763c5c07dbb44738fcb01fe301e5d1a
Kilian <96377170+kilianmendez@users.noreply.github.com>
2024-10-25 17:37:35 +0200

    Primer Commit
    Primer push Backend Asp.net (Login y Register)

---
+++ SolessBackend/SolessBackend/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SolessBackend.Interfaces;
+using SolessBackend.Models;
+
+namespace SolessBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository) 
+        { 
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers() 
+        {
+            var users = _userRepository.GetUsersAsync;
+
+            if (!ModelState.IsValid) 
+            { 
+                return BadRequest(ModelState);
+            }
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUser(int id) 
+        { 
+            var user = _userRepository.GetUserByIdAsync(id);
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult AddUser([FromBody] User userToAdd)
+        {
+            _userRepository.AddUserAsync(userToAdd);
+            
+            return Created();
+        }
+
+    }
+}
